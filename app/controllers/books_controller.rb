9bbe23f664d7bea98cf1bb9a864215369ad3d1e3
KielRodriguez <kr.rdz.20@gmail.com>
2018-02-14 16:53:00 -0600

    recurso books para respuesta xls y csv

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,11 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
+
+    respond_to do |format|
+      format.html
+      format.csv { send_data @books.to_csv }
+      format.xls { send_data @books.to_csv(col_sep: "\t")}
+    end
   end
 end
